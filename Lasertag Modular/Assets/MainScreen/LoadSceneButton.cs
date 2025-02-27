d03dd7c9dd6fb6652eb4a74ca36bfcf70e6ae55b
abel pujol <abel.pujol@enti.cat>
2025-02-27 20:30:04 +0100

    Setup Scene

---
+++ Lasertag Modular/Assets/MainScreen/LoadSceneButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainScreen
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        [SerializeField] private string _sceneName;
+
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+    }
+}
