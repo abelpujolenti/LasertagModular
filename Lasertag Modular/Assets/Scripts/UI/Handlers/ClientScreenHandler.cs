f2c77bf2023ff03a08fb4dc1236acf7297ad2b5c
Pedro González Navarro <pedro.gonzalez@enti.cat>
2025-05-29 17:16:30 +0200

    Base done!

--- Lasertag Modular/Assets/Scripts/UI/Handlers/ClientScreenHandler.cs
+++ Lasertag Modular/Assets/Scripts/UI/Handlers/ClientScreenHandler.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using Network.Packets;
 using TMPro;
 using UI.Agent;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClientScreenHandler : MonoBehaviour
 {
@@ -17,6 +22,15 @@ public class ClientScreenHandler : MonoBehaviour
     public int ConfirmedPlayers;
     public int AllPlayers;
 
+    [Header("InGame")]
+    public List<GameObject> hearts;
+    public TMP_Text PlayerName;
+    public TMP_Text AScore;
+    public TMP_Text BScore;
+    public GameObject Skill01;
+    public GameObject Skill02;
+    public GameObject Skill03;
+
     private void Start()
     {
         WaitingForNFC.SetActive(true);
@@ -27,5 +41,57 @@ public class ClientScreenHandler : MonoBehaviour
     public void UpdateMatchState()
     {
         MatchPreparationValue.text = ConfirmedPlayers + "/" + AllPlayers;
+    }
+
+    public void SetPlayerName(string newName)
+    {
+        PlayerName.text = newName;
+    }
+
+    public void SetSkills(/*Skill skill01, Skill skill02, Skill skill03*/)
+    {
+        /*if (skill01 != null)
+        {
+            Skill01.GetComponent<Button>().onClick.AddListener(() => skill01.TryUseAbility());
+            Skill01.GetComponentInChildren<TMP_Text>().SetText(skill01._name);
+        }
+        else
+        {
+            Skill01.SetActive(false);
+        }
+        if (skill02 != null)
+        {
+            Skill02.GetComponent<Button>().onClick.AddListener(() => skill02.TryUseAbility());
+            Skill02.GetComponentInChildren<TMP_Text>().SetText(skill02._name);
+        }
+        else 
+        { 
+            Skill02.SetActive(false);
+        }
+        if (skill03 != null)
+        {
+            Skill03.GetComponent<Button>().onClick.AddListener(() => skill03.TryUseAbility());
+            Skill03.GetComponentInChildren<TMP_Text>().SetText(skill03._name);
+        }
+        else
+        {
+            Skill03.SetActive(false);
+        }*/
+    }
+
+    public void UpdateScore(int ANewScore, int BNewScore)
+    {
+        AScore.text = ANewScore.ToString();
+        BScore.text = BNewScore.ToString();
+    }
+
+    public void UpdateHealth(int health)
+    {
+        int heartCounter = 1;
+        foreach (var heart in hearts)
+        {
+            heart.SetActive(heartCounter <= health);
+            heartCounter++;
+        }
     }
 }
